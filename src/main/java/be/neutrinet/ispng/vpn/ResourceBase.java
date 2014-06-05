8ede80f628f5292a3d06cda896b8cc36c7f04ef1
Wannes <wannes321@gmail.com>
2014-06-05 19:39:43 +0200

    ResourceBase cleanup

--- src/main/java/be/neutrinet/ispng/vpn/ResourceBase.java
+++ src/main/java/be/neutrinet/ispng/vpn/ResourceBase.java
@@ -6,29 +6,16 @@
 package be.neutrinet.ispng.vpn;
 
 import java.util.concurrent.ConcurrentMap;
-import org.apache.log4j.Logger;
-import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
-import org.bouncycastle.crypto.params.RSAKeyParameters;
-import org.bouncycastle.crypto.util.PublicKeyFactory;
-import org.bouncycastle.jce.PKCS10CertificationRequest;
-import org.bouncycastle.openssl.PEMReader;
 import org.restlet.Message;
 import org.restlet.data.Status;
 import org.restlet.engine.header.Header;
 import org.restlet.ext.jackson.JacksonRepresentation;
 import org.restlet.representation.Representation;
-import org.restlet.representation.StreamRepresentation;
 import org.restlet.resource.Options;
-import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 import org.restlet.util.Series;
 
 /**
- * Initial simple mapping
- *
- * TODO: Provide a meaningful object representation, e.g. instead of addressId
- * (int) -> address (String)
- *
  * @author double-u
  */
 public abstract class ResourceBase extends ServerResource {
@@ -37,33 +24,6 @@ public abstract class ResourceBase extends ServerResource {
 
     protected final static JacksonRepresentation DEFAULT_ERROR = new JacksonRepresentation(new ClientError("UNKNOWN_ERROR"));
     protected final static JacksonRepresentation DEFAULT_SUCCESS = new JacksonRepresentation("OK");
-
-    @Post
-    public Representation signCSR(Representation csr) {
-        StreamRepresentation sr = (StreamRepresentation) csr;
-        // Authenticate and authorize request
-        // Do all kinds of security checks
-        try {
-            PEMReader reader = new PEMReader(sr.getReader());
-            PKCS10CertificationRequest req = (PKCS10CertificationRequest) reader.readObject();
-            // This makes the NSA work harder on their quantum computer
-            // Require 4096 bit key
-            SubjectPublicKeyInfo pkInfo = req.getCertificationRequestInfo().getSubjectPublicKeyInfo();
-            RSAKeyParameters rsa = (RSAKeyParameters) PublicKeyFactory.createKey(pkInfo);
-            // http://stackoverflow.com/a/20622933
-            if (!(rsa.getModulus().bitLength() > 2048)) {
-                ClientError err = new ClientError("ILLEGAL_KEY_SIZE");
-                return new JacksonRepresentation(err);
-            }
-
-            //ExtendedKeyUsage eku = new ExtendedKeyUsage(value);
-            System.out.println("");
-        } catch (Exception ex) {
-            Logger.getLogger(getClass()).error("Failed to validate CSR", ex);
-        }
-
-        return DEFAULT_ERROR;
-    }
 
     @SuppressWarnings("unchecked")
     static Series<Header> getMessageHeaders(Message message) {
@@ -100,11 +60,6 @@ public abstract class ResourceBase extends ServerResource {
 
     @Options
     public void doOptions(Representation entity) {
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Origin", "*");
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Methods", "POST,OPTIONS");
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Methods", "POST,OPTIONS");
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Headers", "Content-Type");
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Credentials", "false");
-        getMessageHeaders(getResponse()).add("Access-Control-Max-Age", "60");
+        setCORSHeaders(entity);
     }
 }
