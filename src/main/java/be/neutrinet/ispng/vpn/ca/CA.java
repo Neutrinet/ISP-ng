c7f2d5a869b1b7f56d4de2a217f71b04e16dfddf
Wannes <wannes321@gmail.com>
2014-07-04 20:07:52 +0200

    Add certificate signing logic to CA + fixes

--- src/main/java/be/neutrinet/ispng/vpn/ca/CA.java
+++ src/main/java/be/neutrinet/ispng/vpn/ca/CA.java
@@ -1,83 +1,101 @@
 package be.neutrinet.ispng.vpn.ca;
 
-import java.security.*;
-import java.io.*;
-import java.util.Date;
-import java.math.BigInteger;
-import java.security.cert.X509Certificate;
+import be.neutrinet.ispng.DateUtil;
+import be.neutrinet.ispng.VPN;
+import be.neutrinet.ispng.vpn.api.UserCertificate;
+import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.*;
-import org.bouncycastle.asn1.x500.*;
-import org.bouncycastle.asn1.pkcs.*;
-import org.bouncycastle.openssl.*;
-import org.bouncycastle.pkcs.*;
-import org.bouncycastle.cert.*;
-import org.bouncycastle.cms.*;
-import org.bouncycastle.cms.jcajce.*;
-import org.bouncycastle.crypto.util.*;
-import org.bouncycastle.operator.*;
-import org.bouncycastle.operator.bc.*;
-import org.bouncycastle.operator.jcajce.*;
-import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.crypto.util.PrivateKeyFactory;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+
+import java.io.FileInputStream;
+import java.math.BigInteger;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class CA {
 
-    public final static String SIGNING_ALGO = "SHA512withRSA";
-    public final static int KEY_SIZE = 4096;
+    public final static String SIGNING_ALGORITHM = "SHA512withRSA";
 
-    /**
-     * http://stackoverflow.com/questions/7230330/sign-csr-using-bouncy-castle
-     * Given a Keystore containing a private key and certificate and a Reader
-     * containing a PEM-encoded Certificiate Signing Request (CSR), sign the CSR
-     * with that private key and return the signed certificate as a PEM-encoded
-     * PKCS#7 signedData object. The returned value can be written to a file and
-     * imported into a Java KeyStore with "keytool -import -trustcacerts -alias
-     * subjectalias -file file.pem"
-     *
-     * @param pemcsr a Reader from which will be read a PEM-encoded CSR (begins
-     * "-----BEGIN NEW CERTIFICATE REQUEST-----")
-     * @param validity the number of days to sign the Certificate for
-     * @param keystore the KeyStore containing the CA signing key
-     * @param alias the alias of the CA signing key in the KeyStore
-     * @param password the password of the CA signing key in the KeyStore
-     *
-     * @return a String containing the PEM-encoded signed Certificate (begins
-     * "-----BEGIN PKCS #7 SIGNED DATA-----")
-     */
-    protected String signCSR(Reader pemcsr, int validity, KeyStore keystore, String alias, char[] password) throws Exception {
-        PrivateKey cakey = (PrivateKey) keystore.getKey(alias, password);
-        X509Certificate cacert = (X509Certificate) keystore.getCertificate(alias);
-        PEMReader reader = new PEMReader(pemcsr);
-        PKCS10CertificationRequest csr = new PKCS10CertificationRequest((CertificationRequest) reader.readObject());
+    private KeyStore keyStore;
+    private PrivateKey caKey;
+    private X509Certificate caCert;
 
-        AlgorithmIdentifier sigAlgId = new DefaultSignatureAlgorithmIdentifierFinder().find(SIGNING_ALGO);
-        AlgorithmIdentifier digAlgId = new DefaultDigestAlgorithmIdentifierFinder().find(sigAlgId);
-        X500Name issuer = new X500Name(cacert.getSubjectX500Principal().getName());
-        BigInteger serial = new BigInteger(32, new SecureRandom());
-        Date from = new Date();
-        Date to = new Date(System.currentTimeMillis() + (validity * 86400000L));
+    public CA() {
+        try {
+            keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(new FileInputStream(VPN.cfg.getProperty("ca.keyStore")),
+                    VPN.cfg.get("ca.keyStorePassword").toString().toCharArray());
+            caKey = (PrivateKey) keyStore.getKey("ca", VPN.cfg.get("ca.keyPassword").toString().toCharArray());
+            caCert = (X509Certificate) keyStore.getCertificate("ca");
+        } catch (Exception ex) {
+            Logger.getLogger(UserCertificate.class).error("Failed to load ca key", ex);
+        }
+    }
 
-        X509v3CertificateBuilder certgen = new X509v3CertificateBuilder(issuer, serial, from, to, csr.getSubject(), csr.getSubjectPublicKeyInfo());
-        certgen.addExtension(X509Extension.basicConstraints, false, new BasicConstraints(false));
-        certgen.addExtension(X509Extension.subjectKeyIdentifier, false, new SubjectKeyIdentifier(csr.getSubjectPublicKeyInfo()));
-        certgen.addExtension(X509Extension.authorityKeyIdentifier, false, new AuthorityKeyIdentifier(new GeneralNames(new GeneralName(new X509Name(cacert.getSubjectX500Principal().getName()))), cacert.getSerialNumber()));
+    // loosely based on http://stackoverflow.com/questions/7230330/sign-csr-using-bouncy-castle
+    // returns signed certificate in DER format
+    protected byte[] signCSR(PKCS10CertificationRequest csr, int daysValid) throws Exception {
+        try {
+            // Certificate serials should be random (hash)
+            //http://crypto.stackexchange.com/questions/257/unpredictability-of-x-509-serial-numbers
+            SecureRandom random = new SecureRandom();
+            byte[] serial = new byte[16];
+            random.nextBytes(serial);
+            BigInteger bigserial = new BigInteger(serial);
 
-        ContentSigner signer = new BcRSAContentSignerBuilder(sigAlgId, digAlgId).build(PrivateKeyFactory.createKey(cakey.getEncoded()));
-        X509CertificateHolder holder = certgen.build(signer);
-        byte[] certencoded = holder.toASN1Structure().getEncoded();
+            // One year certificate validity
+            LocalDateTime expirationDate = LocalDateTime.now().plusDays(daysValid);
 
-        CMSSignedDataGenerator generator = new CMSSignedDataGenerator();
-        signer = new JcaContentSignerBuilder(SIGNING_ALGO).build(cakey);
-        generator.addSignerInfoGenerator(new JcaSignerInfoGeneratorBuilder(new JcaDigestCalculatorProviderBuilder().build()).build(signer, cacert));
-        generator.addCertificate(new X509CertificateHolder(certencoded));
-        generator.addCertificate(new X509CertificateHolder(cacert.getEncoded()));
-        CMSTypedData content = new CMSProcessableByteArray(certencoded);
-        CMSSignedData signeddata = generator.generate(content, true);
+            AlgorithmIdentifier sigAlgId = new DefaultSignatureAlgorithmIdentifierFinder().find(SIGNING_ALGORITHM);
+            AlgorithmIdentifier digAlgId = new DefaultDigestAlgorithmIdentifierFinder().find(sigAlgId);
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        out.write("-----BEGIN PKCS #7 SIGNED DATA-----\n".getBytes("ISO-8859-1"));
-        out.write(Base64.encode(signeddata.getEncoded()));
-        out.write("\n-----END PKCS #7 SIGNED DATA-----\n".getBytes("ISO-8859-1"));
-        out.close();
-        return new String(out.toByteArray(), "ISO-8859-1");
+            X509v3CertificateBuilder certgen = new X509v3CertificateBuilder(
+                    new X500Name("C=BE, ST=Brussels Capital Region, L=Brussels, "
+                            + "O=Neutrinet, OU=CA, CN=ca.neutrinet.be/"
+                            + "name=Neutrinet User Certificate Authority/"
+                            + "emailAddress=contact@neutrinet.be"),
+                    bigserial,
+                    new Date(),
+                    DateUtil.convert(expirationDate),
+                    X500Name.getInstance(csr.getSubject()),
+                    csr.getSubjectPublicKeyInfo());
+
+            // Constraints and usage
+            BasicConstraints basicConstraints = new BasicConstraints(false);
+            ExtendedKeyUsage eku = new ExtendedKeyUsage(KeyPurposeId.id_kp_clientAuth);
+
+            certgen.addExtension(X509Extension.basicConstraints, false, basicConstraints);
+            certgen.addExtension(X509Extension.extendedKeyUsage, false, eku);
+
+            // Identifiers
+            SubjectKeyIdentifier subjectKeyIdentifier = new SubjectKeyIdentifier(csr.getSubjectPublicKeyInfo());
+            AuthorityKeyIdentifier authorityKeyIdentifier = new AuthorityKeyIdentifier(new GeneralNames
+                    (new GeneralName(new X500Name(caCert.getSubjectX500Principal().getName()))), caCert.getSerialNumber());
+
+            certgen.addExtension(X509Extension.subjectKeyIdentifier, false, subjectKeyIdentifier);
+            certgen.addExtension(X509Extension.authorityKeyIdentifier, false, authorityKeyIdentifier);
+
+            ContentSigner signer = new BcRSAContentSignerBuilder(sigAlgId, digAlgId).build(PrivateKeyFactory.createKey(caKey.getEncoded()));
+            X509CertificateHolder holder = certgen.build(signer);
+            byte[] certencoded = holder.toASN1Structure().getEncoded();
+
+            return certencoded;
+        } catch (Exception ex) {
+            Logger.getLogger(getClass()).error("Failed to validate CSR and sign CSR", ex);
+        }
+
+        return null;
     }
 }
