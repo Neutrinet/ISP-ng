98f99f266c3c19c057bf2057fe0309d5e356956a
Wannes <wannes@neutrinet.be>
2014-08-24 20:09:43 +0200

    Runtime config pt.2

--- src/main/java/be/neutrinet/ispng/config/Config.java
+++ src/main/java/be/neutrinet/ispng/config/Config.java
@@ -1,16 +1,110 @@
 package be.neutrinet.ispng.config;
 
+import be.neutrinet.ispng.VPN;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorWatcher;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.log4j.Logger;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.data.Stat;
+
+import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+
 /**
  * Created by wannes on 24/08/14.
  */
 public class Config {
     private final static Config instance = new Config();
+    private CuratorFramework cf;
+    private final static Charset CHARSET = Charset.forName("UTF-8");
+    private final static String PREFIX = "/ispng/";
 
     private Config() {
 
     }
 
-    public Config get() {
+    public final void boot() {
+        ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        cf = CuratorFrameworkFactory.newClient(VPN.cfg.getProperty("zookeeper.connectionString").toString(), retryPolicy);
+        cf.start();
+
+        for (Map.Entry<Object, Object> entry : VPN.cfg.entrySet()) {
+            write(entry.getKey().toString().replace(".", "/"), entry.getValue().toString());
+        }
+    }
+
+    public static Config get() {
         return instance;
+    }
+
+    public static Optional<String> get(String key) {
+        return instance.getValue(key);
+    }
+
+    public Optional<String> getValue(String key) {
+        try {
+            byte[] value = cf.getData().forPath(PREFIX + key);
+            return Optional.ofNullable(new String(value, CHARSET));
+        } catch (Exception ex) {
+            Logger.getLogger(getClass()).error("Failed to read config from ZeeKeeper", ex);
+        }
+
+        return null;
+    }
+
+    public void write(String key, String value) {
+        try {
+            Stat stat = cf.checkExists().forPath(PREFIX + key);
+            if (stat == null) {
+                cf.create().creatingParentsIfNeeded().forPath(PREFIX + key, value.getBytes(CHARSET));
+            } else {
+                cf.setData().forPath(PREFIX + key, value.getBytes(CHARSET));
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(getClass()).error("Failed to write config to ZooKeeper", ex);
+        }
+    }
+
+    public void watch(String key, Consumer<String> listener) {
+        try {
+            CuratorWatcher cw = (WatchedEvent watchedEvent) -> {
+                if (watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged) {
+                    byte[] value = cf.getData().forPath(watchedEvent.getPath());
+                    listener.accept(new String(value, CHARSET));
+                }
+            };
+
+            Stat stat = cf.checkExists().forPath(PREFIX + key);
+            if (stat == null)
+                cf.create().creatingParentsIfNeeded().forPath(PREFIX + key, null);
+            cf.getData().usingWatcher(cw).forPath(PREFIX + key);
+        } catch (Exception ex) {
+            Logger.getLogger(getClass()).error("Failed to add config watcher", ex);
+        }
+    }
+
+    public void watch(String[] keys, Consumer<String> listener) {
+        try {
+            CuratorWatcher cw = (WatchedEvent watchedEvent) -> {
+                if (watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged) {
+                    byte[] value = cf.getData().forPath(watchedEvent.getPath());
+                    listener.accept(new String(value, CHARSET));
+                }
+            };
+
+            for (String key : keys) {
+                Stat stat = cf.checkExists().forPath(PREFIX + key);
+                if (stat == null)
+                    cf.create().creatingParentsIfNeeded().forPath(PREFIX + key, null);
+                cf.getData().usingWatcher(cw).forPath(PREFIX + key);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(getClass()).error("Failed to add config watcher", ex);
+        }
     }
 }
--- src/main/java/be/neutrinet/ispng/config/ConfigWatcher.java
+++ src/main/java/be/neutrinet/ispng/config/ConfigWatcher.java
@@ -4,4 +4,5 @@ package be.neutrinet.ispng.config;
  * Created by wannes on 24/08/14.
  */
 public interface ConfigWatcher {
+    public void valueChanged(String newValue);
 }
