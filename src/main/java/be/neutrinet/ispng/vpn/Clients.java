be3f3498910a6607f2b3a56d831f5146fd6ea6a9
Wannes <wannes321@gmail.com>
2014-10-19 23:52:44 +0200

    Add Clients helper

---
+++ src/main/java/be/neutrinet/ispng/vpn/Clients.java
@@ -0,0 +1,30 @@
+package be.neutrinet.ispng.vpn;
+
+import be.neutrinet.ispng.VPN;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.table.TableUtils;
+import org.apache.log4j.Logger;
+
+import java.sql.SQLException;
+
+/**
+ * Created by wannes on 10/18/14.
+ */
+public class Clients {
+    public final static Client NONE;
+
+    static {
+        try {
+            dao = DaoManager.createDao(VPN.cs, Client.class);
+            TableUtils.createTableIfNotExists(VPN.cs, Client.class);
+        } catch (SQLException ex) {
+            Logger.getLogger(Client.class).error("Failed to create DAO", ex);
+        }
+
+        NONE = new Client();
+        NONE.id = -1;
+    }
+
+    public static Dao<Client, String> dao;
+}
