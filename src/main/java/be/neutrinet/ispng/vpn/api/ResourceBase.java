7b94553d003d772af2e534672c92d9200428ab2b
Wannes <wannes321@gmail.com>
2014-10-19 23:42:53 +0200

    Add update merge helper

--- src/main/java/be/neutrinet/ispng/vpn/api/ResourceBase.java
+++ src/main/java/be/neutrinet/ispng/vpn/api/ResourceBase.java
@@ -6,6 +6,7 @@
 package be.neutrinet.ispng.vpn.api;
 
 import be.neutrinet.ispng.vpn.ClientError;
+import org.apache.log4j.Logger;
 import org.restlet.Message;
 import org.restlet.data.Status;
 import org.restlet.engine.header.Header;
@@ -15,6 +16,8 @@ import org.restlet.resource.Options;
 import org.restlet.resource.ServerResource;
 import org.restlet.util.Series;
 
+import java.lang.reflect.Field;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentMap;
 
 /**
@@ -57,6 +60,29 @@ public abstract class ResourceBase extends ServerResource {
         getMessageHeaders(getResponse()).add("Access-Control-Allow-Headers", "Content-Type");
         getMessageHeaders(getResponse()).add("Access-Control-Allow-Credentials", "false");
         getMessageHeaders(getResponse()).add("Access-Control-Max-Age", "60");
+    }
+
+    protected <T> Optional<T> mergeUpdate(T original, T updated) {
+        if (original == null || updated == null) throw new IllegalArgumentException("Not all arguments were present");
+
+        for (Field f : original.getClass().getFields()) {
+            // Never update ID fields
+            if (f.getName().equals("id")) continue;
+
+            f.setAccessible(true);
+
+            try {
+                Object newValue = f.get(updated);
+                if (newValue == null) continue;
+                f.set(original, newValue);
+            } catch (Exception ex) {
+                Logger.getLogger(getClass()).error("Failed to merge updates");
+                return Optional.empty();
+            }
+
+        }
+
+        return Optional.of(original);
     }
 
     @Options
