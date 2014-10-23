673b58115e0f540c19301d4e63e20c490f062af9
Wannes <wannes321@gmail.com>
2014-10-23 20:21:18 +0200

    Allow multiple IP lease request

--- src/main/java/be/neutrinet/ispng/vpn/api/AddressLease.java
+++ src/main/java/be/neutrinet/ispng/vpn/api/AddressLease.java
@@ -6,18 +6,8 @@
 package be.neutrinet.ispng.vpn.api;
 
 import be.neutrinet.ispng.DateUtil;
-import be.neutrinet.ispng.vpn.ClientError;
-import be.neutrinet.ispng.vpn.IPAddress;
-import be.neutrinet.ispng.vpn.IPAddresses;
-import be.neutrinet.ispng.vpn.ResourceBase;
-import be.neutrinet.ispng.vpn.User;
-import be.neutrinet.ispng.vpn.Users;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import be.neutrinet.ispng.VPN;
+import be.neutrinet.ispng.vpn.*;
 import org.apache.log4j.Logger;
 import org.restlet.data.Status;
 import org.restlet.ext.jackson.JacksonRepresentation;
@@ -25,6 +15,13 @@ import org.restlet.representation.Representation;
 import org.restlet.resource.Delete;
 import org.restlet.resource.Get;
 import org.restlet.resource.Put;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -35,10 +32,10 @@ public class AddressLease extends ResourceBase {
     @Get
     public Representation getAssignedLeases() {
         try {
-            HashMap<User, List<IPAddress>> map = new HashMap<>();
-            List<User> users = Users.dao.queryForAll();
-            for (User u : users) {
-                map.put(u, IPAddresses.dao.queryForEq("user_id", u));
+            HashMap<Client, List<IPAddress>> map = new HashMap<>();
+            List<Client> clients = Clients.dao.queryForAll();
+            for (Client c : clients) {
+                map.put(c, IPAddresses.dao.queryForEq("client_id", c.id));
             }
 
             return new JacksonRepresentation(map);
@@ -51,29 +48,43 @@ public class AddressLease extends ResourceBase {
 
     @Put
     public Representation create(Map<String, Object> data) {
-        int userId = (int) data.get("user");
-        assert userId > 0;
+        int clientId = (int) data.get("client");
+        assert clientId > 0;
         int version = (int) data.get("version");
         assert version == 4 || version == 6;
 
+        int leaseAmount = 1;
+        if (version == 6 && data.containsKey("leaseAmount")) {
+            leaseAmount = (int) data.get("leaseAmount");
+        }
+
+        if (leaseAmount > Integer.parseInt(VPN.cfg.getProperty("vpn.maxLeaseAmount", "4"))) {
+            return clientError("IP_ADDRESS_LEASE_TOO_LARGE", Status.CLIENT_ERROR_NOT_ACCEPTABLE);
+        }
+
         try {
-            List<IPAddress> ownedAddr = IPAddresses.dao.queryForEq("user_id", userId);
-            for (IPAddress ad : ownedAddr) {
-                if (ad.ipVersion == version) {
-                    return new JacksonRepresentation(new ClientError("MAX_IP_ADDRESSES_EXCEEDED"));
+            Client client = Clients.dao.queryForId("" + clientId);
+            List<IPAddress> ipAddresses = IPAddresses.forUser(client.user, version);
+
+            if (ipAddresses.size() > 0) {
+                return new JacksonRepresentation(new ClientError("MAX_IP_ADDRESSES_EXCEEDED"));
+            }
+
+            IPAddress[] addrs = new IPAddress[leaseAmount];
+            for (int i = 0; i < leaseAmount; i++) {
+                IPAddress addr = IPAddresses.findUnused(version);
+                if (addr == null) {
+                    return clientError("OUT_OF_ADDRESSES", Status.SERVER_ERROR_INTERNAL);
                 }
+                addr.leasedAt = new Date();
+                addr.client = Clients.dao.queryForId("" + clientId);
+                addr.expiry = DateUtil.convert(LocalDate.now().plusDays(365L));
+                IPAddresses.dao.update(addr);
+
+                addrs[i] = addr;
             }
 
-            IPAddress addr = IPAddresses.findUnused(version);
-            if (addr == null) {
-                return clientError("OUT_OF_ADDRESSES", Status.SERVER_ERROR_INTERNAL);
-            }
-            addr.leasedAt = new Date();
-            addr.user = Users.dao.queryForId("" + userId);
-            addr.expiry = DateUtil.convert(LocalDate.now().plusDays(365L));
-            IPAddresses.dao.update(addr);
-
-            return new JacksonRepresentation(addr);
+            return new JacksonRepresentation(addrs);
         } catch (SQLException ex) {
             Logger.getLogger(getClass()).error("Failed to create address lease", ex);
         }
@@ -89,7 +100,7 @@ public class AddressLease extends ResourceBase {
             if (data.containsKey("user")) {
                 List<IPAddress> forUser = IPAddresses.forUser(Users.dao.queryForId(data.get("user")), ipVersion);
                 for (IPAddress addr : forUser) {
-                    addr.user = Users.NOBODY;
+                    addr.client = Clients.NONE;
                     IPAddresses.dao.update(addr);
                 }
             }
