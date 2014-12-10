cdcc0f5e1413ce510cb0748efe19073e55d14567
Wannes <wannes321@gmail.com>
2014-12-11 00:37:49 +0100

    Adjust to new Restlet version

--- src/main/java/be/neutrinet/ispng/vpn/api/ResourceBase.java
+++ src/main/java/be/neutrinet/ispng/vpn/api/ResourceBase.java
@@ -7,18 +7,14 @@ package be.neutrinet.ispng.vpn.api;
 
 import be.neutrinet.ispng.vpn.ClientError;
 import org.apache.log4j.Logger;
-import org.restlet.Message;
 import org.restlet.data.Status;
-import org.restlet.engine.header.Header;
 import org.restlet.ext.jackson.JacksonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Options;
 import org.restlet.resource.ServerResource;
-import org.restlet.util.Series;
 
 import java.lang.reflect.Field;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author wannes
@@ -27,21 +23,6 @@ public abstract class ResourceBase extends ServerResource {
 
     protected final static JacksonRepresentation DEFAULT_ERROR = new JacksonRepresentation(new ClientError("UNKNOWN_ERROR"));
     protected final static JacksonRepresentation DEFAULT_SUCCESS = new JacksonRepresentation("OK");
-    private static final String HEADERS_KEY = "org.restlet.http.headers";
-
-    @SuppressWarnings("unchecked")
-    static Series<Header> getMessageHeaders(Message message) {
-        ConcurrentMap<String, Object> attrs = message.getAttributes();
-        Series<Header> headers = (Series<Header>) attrs.get(HEADERS_KEY);
-        if (headers == null) {
-            headers = new Series<>(Header.class);
-            Series<Header> prev = (Series<Header>) attrs.putIfAbsent(HEADERS_KEY, headers);
-            if (prev != null) {
-                headers = prev;
-            }
-        }
-        return headers;
-    }
 
     protected JacksonRepresentation error() {
         setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
@@ -54,12 +35,7 @@ public abstract class ResourceBase extends ServerResource {
     }
 
     protected void setCORSHeaders(Representation entity) {
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Origin", "*");
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Methods", "POST,OPTIONS");
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Methods", "POST,OPTIONS");
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Headers", "Content-Type");
-        getMessageHeaders(getResponse()).add("Access-Control-Allow-Credentials", "false");
-        getMessageHeaders(getResponse()).add("Access-Control-Max-Age", "60");
+        getResponse().setAccessControlAllowOrigin("*");
     }
 
     protected <T> Optional<T> mergeUpdate(T original, T updated) {
