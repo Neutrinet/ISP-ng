9ce8f95d95abeb48bb585a5a154bd61349843258
Wannes <wannes321@gmail.com>
2015-01-16 21:52:02 +0100

    Activate new security infra

--- src/main/java/be/neutrinet/ispng/RestletServlet.java
+++ src/main/java/be/neutrinet/ispng/RestletServlet.java
@@ -5,6 +5,7 @@
  */
 package be.neutrinet.ispng;
 
+import be.neutrinet.ispng.security.SessionManager;
 import be.neutrinet.ispng.vpn.User;
 import be.neutrinet.ispng.vpn.Users;
 import be.neutrinet.ispng.vpn.admin.Registration;
@@ -12,8 +13,11 @@ import be.neutrinet.ispng.vpn.api.*;
 import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.data.ChallengeScheme;
+import org.restlet.data.Method;
 import org.restlet.data.Status;
+import org.restlet.engine.application.CorsResponseHelper;
 import org.restlet.ext.servlet.ServletAdapter;
+import org.restlet.routing.Filter;
 import org.restlet.routing.Router;
 import org.restlet.routing.Template;
 import org.restlet.security.ChallengeAuthenticator;
@@ -27,7 +31,6 @@ import java.io.IOException;
 import java.util.UUID;
 
 /**
- *
  * @author wannes
  */
 public class RestletServlet extends HttpServlet {
@@ -52,15 +55,15 @@ public class RestletServlet extends HttpServlet {
         router.attach("/client/{client}/config", VPNClientConfig.class);
         router.attach("/client/{client}", VPNClient.class);
         router.attach("/user/{user}/setting/{setting}", UserSettings.class);
-        router.attach("/user/{user}", UserManagement.class);
         router.attach("/user/login", UserLogin.class);
+        router.attach("/user/{user}", UserManagement.class);
 
         ChallengeAuthenticator auth = new ChallengeAuthenticator(this.adapter.getContext(), ChallengeScheme.HTTP_BASIC, "Neutrinet API") {
 
             @Override
             protected int beforeHandle(Request request, Response response) {
-
-                if (request.getResourceRef().getPath().startsWith("/api/reg/")) {
+                if (request.getResourceRef().getPath().startsWith("/api/reg/") ||
+                        request.getResourceRef().getPath().startsWith("/api/user/login")) {
                     response.setStatus(Status.SUCCESS_OK);
                     return CONTINUE;
                 }
@@ -75,14 +78,40 @@ public class RestletServlet extends HttpServlet {
 
                 return super.beforeHandle(request, response);
             }
-
         };
 
         auth.setVerifier(new SecretVerifier() {
+            private User user = null;
+
+            @Override
+            public int verify(Request request, Response response) {
+                int result = super.verify(request, response);
+
+                String sessionToken = null;
+                if (request.getCookies().getFirst("Session") != null) {
+                    sessionToken = request.getCookies().getFirstValue("Session");
+                } else if (request.getHeaders().getFirstValue("Session") != null) {
+                    sessionToken = request.getHeaders().getFirstValue("Session");
+                }
+
+                if (sessionToken != null) {
+                    if (!SessionManager.validateToken(sessionToken, request.getClientInfo().getAddress())) {
+                        response.setStatus(Status.CLIENT_ERROR_FORBIDDEN);
+                        return RESULT_INVALID;
+                    } else {
+                        return RESULT_VALID;
+                    }
+                } else {
+                    assert user != null;
+                    response.setLocationRef("login");
+                }
+
+                return result;
+            }
 
             @Override
             public int verify(String identifier, char[] secret) {
-                User user = Users.authenticate(identifier, new String(secret));
+                user = Users.authenticate(identifier, new String(secret));
                 if (user != null) {
                     return RESULT_VALID;
                 } else {
@@ -92,7 +121,32 @@ public class RestletServlet extends HttpServlet {
         });
         auth.setNext(router);
 
-        this.adapter.setNext(auth);
+        Filter corsFilter = new Filter(this.adapter.getContext()) {
+            @Override
+            protected int beforeHandle(Request request, Response response) {
+                response.getAllowedMethods().add(Method.DELETE);
+                response.getAllowedMethods().add(Method.GET);
+                response.getAllowedMethods().add(Method.HEAD);
+                response.getAllowedMethods().add(Method.POST);
+                response.getAllowedMethods().add(Method.PUT);
+
+                // F*CK ME CORS FINALLY WORKS
+                // WORST DOC EVER
+                CorsResponseHelper corsResponseHelper = new CorsResponseHelper();
+                corsResponseHelper.allowedCredentials = true;
+                corsResponseHelper.allowAllRequestedHeaders = true;
+                corsResponseHelper.addCorsResponseHeaders(request, response);
+
+                if (request.getMethod().equals(Method.OPTIONS)) {
+                    return STOP;
+                }
+
+                return CONTINUE;
+            }
+        };
+        corsFilter.setNext(auth);
+
+        this.adapter.setNext(corsFilter);
     }
 
     @Override
