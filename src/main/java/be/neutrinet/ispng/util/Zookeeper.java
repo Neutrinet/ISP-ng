8cdaff4342ce8c12036755d85716149b8bcbfff4
Wannes <wannes321@gmail.com>
2015-02-03 21:04:55 +0100

    Added ensurePathExists helper to Zookeeper

--- src/main/java/be/neutrinet/ispng/util/Zookeeper.java
+++ src/main/java/be/neutrinet/ispng/util/Zookeeper.java
@@ -3,7 +3,9 @@ package be.neutrinet.ispng.util;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.log4j.Logger;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 /**
  * Created by wannes on 1/30/15.
@@ -24,5 +26,15 @@ public class Zookeeper {
 
     public static ZooKeeper getZK() throws Exception {
         return get().getZookeeperClient().getZooKeeper();
+    }
+
+    public static void ensurePathExists(String path) {
+        try {
+            Stat stat = cf.checkExists().forPath(path);
+            if (stat == null)
+                cf.create().creatingParentsIfNeeded().forPath(path, null);
+        } catch (Exception ex) {
+            Logger.getLogger(Zookeeper.class).error("Failed to create path " + path, ex);
+        }
     }
 }
