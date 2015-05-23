6e7ffc854442714fdaeaee354c889219d712ced3
Laurent Peuch <cortex@worlddomination.be>
2015-05-23 22:56:46 +0200

    [fix] api has changed

--- dev/front_endl_mocker/mocker.py
+++ dev/front_endl_mocker/mocker.py
@@ -9,6 +9,46 @@ app = Flask(__name__,
             static_url_path="")
 
 
+@app.route("/api/reg/start", methods=['POST'])
+def start():
+    # test data
+    if request.json["email"] != "test@test.com":
+        return json.dumps({
+            "errorKey": "INVALID_UNLOCK_KEY",
+            "message": "The unlock key used is invalid or has been used before",
+            "url": None
+        })
+
+    return json.dumps({
+        "user":
+        {
+            "id": 0,
+            "email": "test@test.com",
+            "name": None,
+            "lastName": None,
+            "street": None,
+            "postalCode": None,
+            "municipality": None,
+            "birthPlace": None,
+            "birthDate": None,
+            "enabled": False,
+            "certId": None,
+            "country": None
+        },
+        "timeInitiated": 1413231972887,
+        "ipv4Id": 0,
+        "ipv6Id": 0,
+        "unlockKey":
+        {
+            "key": "pouet",
+            "email": "test@test.com",
+            "usedAt": None
+        },
+        "completed": None,
+        "id": "7ed0cea2-1eef-43a9-b7db-b372aa67700f"
+    })
+
+
 @app.route("/api/reg/validateKey", methods=['POST'])
 def validate_key():
     # test data
